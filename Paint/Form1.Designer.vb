5b62fd2a17df56d234c18e570dd763ecf1faeebc
Braydon <blamoreaux4@gmail.com>
2022-03-10 12:48:34 -0700

    Basic Paint Program with save and clear button
    Paint Setup

---
+++ Paint/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.organgeButton = New System.Windows.Forms.Button()
+        Me.redButton = New System.Windows.Forms.Button()
+        Me.WidthTrack = New System.Windows.Forms.TrackBar()
+        Me.CustomColorButton = New System.Windows.Forms.Button()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.greenButton = New System.Windows.Forms.Button()
+        Me.yellowButton = New System.Windows.Forms.Button()
+        Me.whiteButton = New System.Windows.Forms.Button()
+        Me.blackButton = New System.Windows.Forms.Button()
+        Me.purpleButton = New System.Windows.Forms.Button()
+        Me.blueButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.saveButton = New System.Windows.Forms.Button()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        CType(Me.WidthTrack, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(712, 285)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Width:"
+        '
+        'organgeButton
+        '
+        Me.organgeButton.BackColor = System.Drawing.Color.Orange
+        Me.organgeButton.Location = New System.Drawing.Point(756, 15)
+        Me.organgeButton.Name = "organgeButton"
+        Me.organgeButton.Size = New System.Drawing.Size(26, 25)
+        Me.organgeButton.TabIndex = 10
+        Me.organgeButton.UseVisualStyleBackColor = False
+        '
+        'redButton
+        '
+        Me.redButton.BackColor = System.Drawing.Color.Red
+        Me.redButton.Location = New System.Drawing.Point(678, 15)
+        Me.redButton.Name = "redButton"
+        Me.redButton.Size = New System.Drawing.Size(26, 25)
+        Me.redButton.TabIndex = 9
+        Me.redButton.UseVisualStyleBackColor = False
+        '
+        'WidthTrack
+        '
+        Me.WidthTrack.Location = New System.Drawing.Point(678, 301)
+        Me.WidthTrack.Name = "WidthTrack"
+        Me.WidthTrack.Size = New System.Drawing.Size(104, 45)
+        Me.WidthTrack.TabIndex = 8
+        '
+        'CustomColorButton
+        '
+        Me.CustomColorButton.Location = New System.Drawing.Point(678, 235)
+        Me.CustomColorButton.Name = "CustomColorButton"
+        Me.CustomColorButton.Size = New System.Drawing.Size(104, 25)
+        Me.CustomColorButton.TabIndex = 7
+        Me.CustomColorButton.Text = "Custom Color"
+        Me.CustomColorButton.UseVisualStyleBackColor = True
+        '
+        'greenButton
+        '
+        Me.greenButton.BackColor = System.Drawing.Color.Green
+        Me.greenButton.Location = New System.Drawing.Point(756, 73)
+        Me.greenButton.Name = "greenButton"
+        Me.greenButton.Size = New System.Drawing.Size(26, 25)
+        Me.greenButton.TabIndex = 13
+        Me.greenButton.UseVisualStyleBackColor = False
+        '
+        'yellowButton
+        '
+        Me.yellowButton.BackColor = System.Drawing.Color.Yellow
+        Me.yellowButton.Location = New System.Drawing.Point(678, 73)
+        Me.yellowButton.Name = "yellowButton"
+        Me.yellowButton.Size = New System.Drawing.Size(26, 25)
+        Me.yellowButton.TabIndex = 12
+        Me.yellowButton.UseVisualStyleBackColor = False
+        '
+        'whiteButton
+        '
+        Me.whiteButton.BackColor = System.Drawing.Color.White
+        Me.whiteButton.Location = New System.Drawing.Point(756, 188)
+        Me.whiteButton.Name = "whiteButton"
+        Me.whiteButton.Size = New System.Drawing.Size(26, 25)
+        Me.whiteButton.TabIndex = 17
+        Me.whiteButton.UseVisualStyleBackColor = False
+        '
+        'blackButton
+        '
+        Me.blackButton.BackColor = System.Drawing.Color.Black
+        Me.blackButton.Location = New System.Drawing.Point(678, 188)
+        Me.blackButton.Name = "blackButton"
+        Me.blackButton.Size = New System.Drawing.Size(26, 25)
+        Me.blackButton.TabIndex = 16
+        Me.blackButton.UseVisualStyleBackColor = False
+        '
+        'purpleButton
+        '
+        Me.purpleButton.BackColor = System.Drawing.Color.Purple
+        Me.purpleButton.Location = New System.Drawing.Point(756, 130)
+        Me.purpleButton.Name = "purpleButton"
+        Me.purpleButton.Size = New System.Drawing.Size(26, 25)
+        Me.purpleButton.TabIndex = 15
+        Me.purpleButton.UseVisualStyleBackColor = False
+        '
+        'blueButton
+        '
+        Me.blueButton.BackColor = System.Drawing.Color.DodgerBlue
+        Me.blueButton.Location = New System.Drawing.Point(678, 130)
+        Me.blueButton.Name = "blueButton"
+        Me.blueButton.Size = New System.Drawing.Size(26, 25)
+        Me.blueButton.TabIndex = 14
+        Me.blueButton.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(6, 15)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(666, 420)
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'saveButton
+        '
+        Me.saveButton.Location = New System.Drawing.Point(692, 352)
+        Me.saveButton.Name = "saveButton"
+        Me.saveButton.Size = New System.Drawing.Size(75, 23)
+        Me.saveButton.TabIndex = 18
+        Me.saveButton.Text = "Save"
+        Me.saveButton.UseVisualStyleBackColor = True
+        '
+        'clearButton
+        '
+        Me.clearButton.Location = New System.Drawing.Point(692, 397)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(75, 23)
+        Me.clearButton.TabIndex = 19
+        Me.clearButton.Text = "Clear"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.clearButton)
+        Me.Controls.Add(Me.saveButton)
+        Me.Controls.Add(Me.whiteButton)
+        Me.Controls.Add(Me.blackButton)
+        Me.Controls.Add(Me.purpleButton)
+        Me.Controls.Add(Me.blueButton)
+        Me.Controls.Add(Me.greenButton)
+        Me.Controls.Add(Me.yellowButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.organgeButton)
+        Me.Controls.Add(Me.redButton)
+        Me.Controls.Add(Me.WidthTrack)
+        Me.Controls.Add(Me.CustomColorButton)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.WidthTrack, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents organgeButton As Button
+    Friend WithEvents redButton As Button
+    Friend WithEvents WidthTrack As TrackBar
+    Friend WithEvents CustomColorButton As Button
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents greenButton As Button
+    Friend WithEvents yellowButton As Button
+    Friend WithEvents whiteButton As Button
+    Friend WithEvents blackButton As Button
+    Friend WithEvents purpleButton As Button
+    Friend WithEvents blueButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents saveButton As Button
+    Friend WithEvents clearButton As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
