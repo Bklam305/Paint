b07d95f258d08b462f27a11da02c78c8641b2193
Braydon <blamoreaux4@gmail.com>
2022-03-10 12:49:54 -0700

    Not Form1, Now its Drawing App
    Title Change

--- Paint/Form1.Designer.vb
+++ Paint/Form1.Designer.vb
@@ -185,7 +185,7 @@ Partial Class Form1
         Me.Controls.Add(Me.CustomColorButton)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Drawing App"
         CType(Me.WidthTrack, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
